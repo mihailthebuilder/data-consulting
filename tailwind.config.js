fcc88dcc061d7429e8584a8a746d01cb14389695
Mihail Marian <mihailmarian12@gmail.com>
2023-09-25 20:50:15 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBlue: '#334D88',
+      },
+    },
+  },
+  plugins: [],
+}
